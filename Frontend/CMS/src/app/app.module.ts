3aa36e296faee3935e9a40907a1bc8ff6bae8c2f
Nemanja Momcilovic <hornhuff@hotmail.com>
2022-03-15 19:37:04 +0100

    initial components

--- Frontend/CMS/src/app/app.module.ts
+++ Frontend/CMS/src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { FileComponent } from './components/file/file.component';
+import { FolderComponent } from './components/folder/folder.component';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
+import { ControlsComponent } from './components/controls/controls.component';
+import { ContentComponent } from './components/content/content.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SidebarComponent,
+    FileComponent,
+    FolderComponent,
+    LoginComponent,
+    RegisterComponent,
+    ControlsComponent,
+    ContentComponent
   ],
   imports: [
     BrowserModule,
